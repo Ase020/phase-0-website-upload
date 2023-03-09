9baf913398f6308a12435b80417df75c56e4d7cb
felixNyalenda <felixasejunior@gmail.com>
2023-03-09 10:14:41 +0300

    add script.js, connect to index.html

---
+++ index.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This h2 tag is added by Javascript. Awesome!";
+document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "This is another <p> element tag created and added by JS.";
+document.querySelector("body").appendChild(p);
